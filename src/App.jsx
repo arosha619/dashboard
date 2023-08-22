a39644275877ffcfc451c9cb965b74012b463fbe
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-08-23 00:13:10 +0530

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css';
 import Sidebar from './layout/Sidebar/Sidebar';
-import Content from './layout/Content/Content';
 import UserList from './components/UserList/UserList';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <>
       <div className='app'>
-        <Sidebar />
-        <UserList/>
+        {/* <Sidebar />
+        <UserList/> */}
+        <Login/>
 
-        {/* <Content /> */}
       </div>
     </>
   )
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,93 @@
-import React from 'react'
-import "./Login.css"
+import React, { useState } from "react";
+import "./Login.css";
 
-function Login() {
+const Login = ({ setIsLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState({});
+
+  const errors = {
+    username: "Invalid username",
+    password: "Invalid password",
+    noUsername: "Please enter your username",
+    noPassword: "Please enter your password",
+  };
+
+  const handleSubmit = (e) => {
+    // Prevent page from reloading
+    e.preventDefault();
+
+    if (!username) {
+      // Username input is empty
+      setErrorMessages({ name: "noUsername", message: errors.noUsername });
+      return;
+    }
+
+    if (!password) {
+      // Password input is empty
+      setErrorMessages({ name: "noPassword", message: errors.noPassword });
+      return;
+    }
+
+    // Search for user credentials
+    // const currentUser = database.find((user) => user.username === username);
+
+    if (currentUser) {
+      if (currentUser.password !== password) {
+        // Wrong password
+        setErrorMessages({ name: "password", message: errors.password });
+      } else {
+        // Correct password, log in user
+        setErrorMessages({});
+        setIsLoggedIn(true);
+      }
+    } else {
+      // Username doens't exist in the database
+      setErrorMessages({ name: "username", message: errors.username });
+    }
+  };
+
+  // Render error messages
+  const renderErrorMsg = (name) =>
+    name === errorMessages.name && (
+      <p className="error_msg">{errorMessages.message}</p>
+    );
+
   return (
-    <div className='login'>Login</div>
-  )
-}
+    <div className="Container">
+      <h1 className="title">Sign In</h1>
+      <p className="subtitle">
+        Please log in using your username and password!
+      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="inputs_container">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {renderErrorMsg("username")}
+          {renderErrorMsg("noUsername")}
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {renderErrorMsg("password")}
+          {renderErrorMsg("noPassword")}
+        </div>
+        <input type="submit" value="Log In" className="login_button" />
+      </form>
+      <div className="link_container">
+        <a href="" className="small">
+          Forgot Password?
+        </a>
+      </div>
 
-export default Login
+    </div>
+  );
+};
+
+export default Login;
