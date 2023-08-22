2feb97956ead1b728d0ddbf29f87a8fd6d483b57
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-08-22 23:52:39 +0530

    remove unwanted folders

--- src/layout/Content/Content.jsx
+++ src/layout/Content/Content.jsx
@@ -1,11 +1,11 @@
 import "./Content.css";
-import ContentTop from '../../components/ContentTop/ContentTop';
+// import ContentTop from '../../components/ContentTop/ContentTop';
 import ContentMain from '../../components/ContentMain/ContentMain';
 
 const Content = () => {
   return (
     <div className='main-content'>
-      <ContentTop />
+      {/* <ContentTop /> */}
       <ContentMain />
     </div>
   )
